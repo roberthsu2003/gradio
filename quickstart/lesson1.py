de0ccaed5775def02673da129b40de0eccec59e6
roberthsu2003 <roberthsu2003@gmail.com>
2025-01-21 04:14:48 +0000

    modify

--- quickstart/lesson1.py
+++ quickstart/lesson1.py
@@ -2,12 +2,15 @@ import gradio as gr
 
 def greet(name, intensity):
     return "Hello, " + name + "!" * int(intensity)
+    
 
 demo = gr.Interface(
     fn=greet,
     inputs=["text", "slider"],
     outputs=["text"],
-    examples = [["徐國堂","2"],["徐瑞彤","1"]]
+    examples = [["徐國堂","2"],["徐瑞彤","1"]],
+    title="Greeting and example",
+    flagging_options=None
 )
 
 demo.launch()
